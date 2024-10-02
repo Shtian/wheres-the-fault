b631e37fb3ad857f5995f94cfcfcb2e3039f3799
Stian Haga <hagastian@gmail.com>
2024-10-02 13:20:57 +0200

    chore: remove docs, use slack instead

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import Terminal from "./lib/Terminal/Terminal.svelte";
-  import Docs from "./lib/Docs/Docs.svelte";
   import ButtDetective from "$lib/components/ButtDetective.svelte";
 </script>
 
